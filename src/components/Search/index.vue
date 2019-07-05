21ce13ee202ec7e87ee983963a1473596233f0f0
Yang Xiukun <myang.shannon1122@gmail.com>
2019-07-05 16:00:20 +0800

    添加了数据

--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -3,13 +3,14 @@
 				<div class="search_input">
 					<div class="search_input_wrapper">
 						<i class="iconfont icon-sousuo"></i>
-						<input type="text">
+                        <!-- 添加 v-model 实现数据的双向绑定，拿到输入的数据值 -->
+						<input type="text" v-model="message">
 					</div>					
 				</div>
 				<div class="search_result">
 					<h3>电影/电视剧/综艺</h3>
 					<ul>
-						<li>
+						<!-- <li>
 							<div class="img"><img src="/images/movie_1.jpg"></div>
 							<div class="info">
 								<p><span>无名之辈</span><span>8.5</span></p>
@@ -17,14 +18,14 @@
 								<p>剧情,喜剧,犯罪</p>
 								<p>2018-11-16</p>
 							</div>
-						</li>
-						<li>
-							<div class="img"><img src="/images/movie_1.jpg"></div>
+						</li> -->
+                        <li v-for="item in moviesList" :key="item.id">
+							<div class="img"><img :src="item.img | setWH('128.180')"></div>
 							<div class="info">
-								<p><span>无名之辈</span><span>8.5</span></p>
-								<p>A Cool Fish</p>
-								<p>剧情,喜剧,犯罪</p>
-								<p>2018-11-16</p>
+								<p><span>{{ item.nm }}</span><span>{{ item.sc }}</span></p>
+								<p>{{ item.enm }}</p>
+								<p>{{ item.cat }}</p>
+								<p>{{ item.rt }}</p>
 							</div>
 						</li>
 					</ul>
@@ -34,7 +35,46 @@
 
 <script>
 export default {
-    name : 'Search'
+    name : 'Search',
+    data(){
+        return {
+            message : '',
+            moviesList : []
+        }
+    },
+    methods : {
+        cancelRequest(){
+            if (typeof this.source === 'function') {
+                this.source('终止请求')
+            }
+        }
+    },
+    // 用 watch 监听数据的变化，允许异步操作；
+    watch : {
+        message(newVal){
+            var that = this;
+            this.cancelRequest();
+            // 函数防抖策略，输入结束的时候，再进行搜索；防止多次触发；
+            this.axios.get('/api/searchList?cityId=10&kw='+newVal,{
+                cancelToken: new this.axios.CancelToken(function executor(c) {
+                    that.source = c;
+                })
+            }).then((res)=>{
+                var msg = res.data.msg;
+                var movies = res.data.data.movies;
+                if (msg && movies) {
+                    this.moviesList = res.data.data.movies.list;
+                }
+            }).catch((err)=>{
+                if (this.axios.isCancel(err)) {
+                    console.log('Request canceled', err.message);  //请求如果被取消，这里是返回取消的message
+                } else {
+                    // handle error
+                    console.log(err);
+                }
+            });
+        }
+    }
 }
 </script>
 
